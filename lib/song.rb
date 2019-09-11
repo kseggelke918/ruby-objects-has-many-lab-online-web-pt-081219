f7963b48e27bb9ee6ff5fdab161c629a1e999b25
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-11 20:34:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,15 @@ class Song
   
   def initialize(name)
     @name = name 
-    @@all << self 
+    save 
   end 
   
+  def save
+    @@all << self 
+  end
   
+  def self.all 
+    @@all 
+  end 
   
 end 
